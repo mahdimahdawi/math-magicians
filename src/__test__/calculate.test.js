d68f99aab269854799392229925c4acdec1fe752
mahdimahdawi <mahdimahdawi2018@gmail.com>
2022-09-21 18:15:43 +0430

    Testing of operate and calculate

---
+++ src/__test__/calculate.test.js
@@ -0,0 +1,69 @@
+import calculate from '../logic/calculate';
+
+describe('Test for Caculate', () => {
+  test('Check for the sum calculation', () => {
+    const obj = {
+      total: 10,
+      next: 5,
+      operation: '+',
+    };
+    const btnName = '=';
+    expect(calculate(obj, btnName)).toEqual({
+      next: null,
+      operation: null,
+      total: '15',
+      });
+  });
+  test('Check fot Subraction', () => {
+    const obj = {
+      total: 10,
+      next: 5,
+      operation: '-',
+    };
+    const btnName = '=';
+    expect(calculate(obj, btnName)).toEqual({
+      next: null,
+      operation: null,
+      total: '5',
+    });
+  });
+  test('Check for Multiplication', () => {
+    const obj = {
+      total: 10,
+      next: 5,
+      operation: 'x',
+    };
+    const btnName = '=';
+    expect(calculate(obj, btnName)).toEqual({
+      next: null,
+      operation: null,
+      total: '50',
+    });
+  });
+  test('Check for Division', () => {
+    const obj = {
+      total: 10,
+      next: 5,
+      operation: '÷',
+    };
+    const btnName = '=';
+    expect(calculate(obj, btnName)).toEqual({
+      next: null,
+      operation: null,
+      total: '2',
+    });
+  });
+  test('Check for modulas', () => {
+    const obj = {
+      total: 9,
+      next: 4,
+      operation: '%',
+    };
+    const btnName = '=';
+    expect(calculate(obj, btnName)).toEqual({
+      next: null,
+      operation: null,
+      total: '1',
+    });
+  });
+});
